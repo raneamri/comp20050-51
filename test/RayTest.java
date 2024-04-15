bf070577c9462becdc436be9530e4e91443ff94b
micaiah <micaiah.fubara@ucdconnect.ie>
2024-04-15 14:34:58 +0100

    RayTest commented help

--- test/RayTest.java
+++ test/RayTest.java
@@ -18,11 +18,12 @@ public class RayTest {
   @Test
   void testSixtyReflection() {}
 
-  @Test
-  testOneTwentyReflection() {}
 
   @Test
-  testFullReflection() {}
+  public void testOnetwentyReflection() {}
+
+  @Test
+  public void testFullReflection(){}
 
   @Test
   void testDrawRays() {
@@ -41,3 +42,82 @@ public class RayTest {
    * to you
    */
 }
+
+
+
+
+
+
+
+/*
+
+  @Test
+  public void testSlopeToDirection() {
+    Board board = new Board();
+    Cell[][] cells = Board.getCells();
+    double[] start = {1,2};
+
+    double[] p1 = {0, 0};
+    double[] p2 = {1, 1};
+
+    Ray ray = new Ray(start, cells[1][0], 10);
+    assertEquals(Direction.DOWN_RIGHT, ray.slopeToDirection(p1, p2));
+  }
+
+
+
+
+  @Test
+  public void testDistanceBetween() {
+    Board board = new Board();
+    Group group = board.getBoardGroup();
+    Cell[][] cells = Board.getCells();
+    double[] start = {1,2};
+
+    double[] p1 = {0, 0};
+    double[] p2 = {3, 4};
+    Ray ray = new Ray(start, cells[1][0], 11);
+    assertEquals(5.0, ray.distanceBetween(p1, p2));
+  }
+
+  @Test
+  public void testSixtyReflection() {
+    Board board = new Board();
+    Group group = board.getBoardGroup();
+    Cell[][] cells = Board.getCells();
+    double[] start = {1,2};
+
+    double[] p1 = {0, 0};
+    double[] p2 = {1, 1};
+    Ray ray = new Ray(start, cells[1][0], 11);
+    Direction direction = Direction.LEFT_RIGHT;
+    assertEquals(Direction.UP_RIGHT, ray.sixtyReflection(direction, p1, p2));
+  }
+
+  @Test
+  public void testOnetwentyReflection() {
+    Board board = new Board();
+    Group group = board.getBoardGroup();
+    Cell cells[][] = Board.getCells();
+    double start[] = {1,2};
+
+    double[] p1 = {0, 0};
+    double[] p2 = {1, 1};
+    double[] p3 = {2, 0};
+
+    Ray ray = new Ray(start, cells[1][0], 10);
+    Direction direction = Direction.LEFT_RIGHT;
+    assertEquals(Direction.UP_LEFT, ray.onetwentyReflection(direction, p1, p2, p3));
+  }
+
+  @Test
+  public void testFullReflection() {
+    Board board = new Board();
+    Group group = board.getBoardGroup();
+    Cell[][] cells = Board.getCells();
+    double[] start = {1,2};
+
+    Ray ray = new Ray(start, cells[1][0], 12);
+    Direction direction = Direction.LEFT_RIGHT;
+    assertEquals(Direction.RIGHT_LEFT, ray.fullReflection(direction));
+  }*/
